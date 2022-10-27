ed9db3894248fc302684b43e669e7d9d744fc6e3
jessica8melo <jessica8melo@gmail.com>
2022-10-27 18:53:12 -0300

    product spec updated

--- spec/requests/api/v1/product_spec.rb
+++ spec/requests/api/v1/product_spec.rb
@@ -3,8 +3,12 @@ require 'rails_helper'
 RSpec.describe "Api::V1::Products", type: :request do
   describe "GET /index" do
     before do
-      create(:product, id:1, name: "Adidas")
-      create(:product, id:2, name: "Gucci")
+      create(:category, id:1, name: "Lazer")
+      create(:category, id:2, name: "Móveis")
+      create(:brand, id:1, name: "Penalty")
+      create(:brand, id:2, name: "Castor")
+      create(:product, id:1, name: "Bola de Futebol", price:1, brand_id:1, category_id:1)
+      create(:product, id:2, name: "Cama", price:2, brand_id:2, category_id:2)
     end
     context " when index return" do
       before do
@@ -20,17 +24,109 @@ RSpec.describe "Api::V1::Products", type: :request do
         expect(JSON.parse(response.body)).to eq([{
           'created_at' => eval(Product.find(1).created_at.to_json),
           'id' => 1,
-          'name' => 'Adidas',
+          'name' => 'Bola de Futebol',
+          'price' => 1,
+          'brand_id' => 1,
+          'category_id' => 1,
           'updated_at' => eval(Product.find(1).created_at.to_json)
         },
         {
           'created_at' => eval(Product.find(2).created_at.to_json),
           'id' => 2,
-          'name' => 'Gucci',
+          'name' => 'Cama',
+          'price' => 2,
+          'brand_id' => 2,
+          'category_id' => 2,
           'updated_at' => eval(Product.find(2).created_at.to_json)
         }
         ])
       end
     end
+  end
+
+  describe "GET /show/:id" do
+    let(:product) {create(:product)}
+    context " id exist" do
+        before do
+            get "/api/v1/product/show/#{product.id}"
+        end
+        it " return http status ok" do
+            expect(response).to have_http_status(:ok)
+        end
+    end
+    context " id not found" do
+        before do
+            get "/api/v1/product/show/-1"
+        end
+        it " return http status not found" do
+            expect(response).to have_http_status(:not_found)
+        end
+    end
+  end
+
+  describe "POST /create" do
+      let(:product_params) do
+          attributes_for(:product)
+      end
+      context " params are ok" do
+          it " return http status created" do
+              post "/api/v1/product/create", params: {product: product_params}
+              expect(response).to have_http_status(:created)
+          end
+      end
+      context " with bad params" do
+          it " when params are nil" do
+              product_params = nil
+              post "/api/v1/product/create", params: {product: product_params}
+              expect(response).to have_http_status(:bad_request)
+          end
+          it " when params are not unique" do
+              post "/api/v1/product/create", params: {product: product_params}
+              post "/api/v1/product/create", params: {product: product_params}
+              expect(response).to have_http_status(:bad_request)
+          end
+      end
+  end
+
+  describe "PATCH /update/:id" do
+      let(:product1) {create(:product, name: 'Product1')}
+      let(:product2) {create(:product, name: 'Product2')}
+      let(:product_params) do
+          attributes_for(:product)
+      end
+      context " params are ok" do
+          it " return http status" do
+              patch "/api/v1/product/update/#{product1.id}", params: {product: {name: "Refrigerante"}}
+              expect(response).to have_http_status(:ok)
+          end
+      end
+      context " params are nil" do
+          it " return http status bad_request" do
+              patch "/api/v1/product/update/#{product1.id}", params: {product: {name: nil}}
+              expect(response).to have_http_status(:bad_request)
+          end
+      end
+      context " params are not unique" do
+          it " return http status bad_request" do
+              patch "/api/v1/product/update/#{product1.id}", params: {product: {name: product2.name}}
+              expect(response).to have_http_status(:bad_request)
+          end
+      end
+  end
+
+  describe "DELETE /delete/:id" do
+      let(:product) {create(:product)}
+      context " product exists" do
+          it " return http status ok" do
+              delete "/api/v1/product/delete/#{product.id}"
+              expect(response).to have_http_status(:ok)
+          end
+      end
+      context " product does not exists" do
+          it " return http status bad_request" do
+              delete "/api/v1/product/delete/-1"
+              expect(response).to have_http_status(:bad_request)
+          end
+      end
   end
 end
