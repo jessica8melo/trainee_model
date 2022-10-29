20343f94eb0597ca20fa0ab5f19aab6914a25fa8
jessica8melo <jessica8melo@gmail.com>
2022-10-29 19:17:17 -0300

    admin spec updated

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Admin, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Testing factory" do
+    context "when using standart factory" do
+      it {expect(build(:admin)).to be_valid}
+    end
+  end
+  
+  describe "Validating admin" do
+    context "Validating email" do
+      it "should be invalid if nil" do
+        expect(build(:admin, email: nil)).to be_invalid
+      end
+      it "should be invalid if repetead" do
+        create(:admin, email:"trainee@struct")
+        expect(build(:admin, email: "trainee@struct")).to be_invalid
+      end
+    end
+    
+    context "Validating password" do
+      it "should be invalid if nil" do
+        expect(build(:admin, email: nil)).to be_invalid
+      end
+    end
+  end
 end
