6dfdb0728834d34211cf84ed116328d8ae3345d0
jessica8melo <jessica8melo@gmail.com>
2022-11-02 19:48:04 -0300

    cart controller updated

--- app/controllers/api/v1/cart_controller.rb
+++ app/controllers/api/v1/cart_controller.rb
@@ -1,7 +1,8 @@
 class Api::V1::CartController < ApplicationController
-    acts_as_token_authentication_handler_for User, only:[:create, :delete]
+    
+    acts_as_token_authentication_handler_for User, only:[:create, :delete, :show]
     def create
-        cart = Cart.new(category_params)
+        cart = Cart.new(cart_params)
         cart.save!
         render json: cart, status: :created
     rescue StandardError => e
@@ -14,5 +15,18 @@ class Api::V1::CartController < ApplicationController
         render json: cart, status: :ok
     rescue StandardError => e 
         render json: e, status: :bad_request
+    end
+
+    def show
+        cart = Cart.find(params[:current_user.id])
+        render json: cart, status: :ok
+    rescue StandardError => e
+        render json: e, status: :not_found
+    end
+
+    private
+    
+    def cart_params
+        params.require(:cart).permit(:user_id, :product_id)
     end
 end
